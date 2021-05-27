7aee8f3e8a05c6eca71226edaab446747200000e
sherms77 <sherms77@gmail.com>
2021-05-27 11:56:41 +1000

    modified:   notes.txt 	modified:   test/str-format_test.py

--- test/str-format_test.py
+++ test/str-format_test.py
@@ -1,4 +1,5 @@
-import datetime
+import datetime, os, shutil
+
 # python docs - https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
 
 # dd (day) - %d
@@ -9,11 +10,18 @@ import datetime
 # ss (seconds) - %S
 
 stamp = datetime.datetime.now()
-path = 'test path using str formatting: /home/sherms/{0}'.format(stamp)
-testPath = 'test path using str formatting: /home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp)
 
-print(path) # works
-print(testPath)
+# path = 'test path using str formatting: /home/sherms/{0}'.format(stamp)
+# testPath = 'test path using str formatting: /home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp)
 
-# format timedate to: dd-mm-yy hh:mm:ss
-# 240521: formatted timedate - refer to testPath var.
+# print(path) # works
+# print(testPath)
+
+# format timedate to: dd-mm-yy hh:mm:ss - 240521: formatted timedate - refer to testPath var.
+# 260521: need to copy folder to new folder with time date stamp
+
+# shutil.copytree('/home/sherms/test folder', '/home/sherms/test1') # works
+
+shutil.copytree('/home/sherms/test folder', '/home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp)) # works
+
+
