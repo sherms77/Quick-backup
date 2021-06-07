8cd94d7b5018dd82db7ffcdf5ff81765c02ab09a
sherms77 <sherms77@gmail.com>
2021-06-07 11:45:21 +1000

    modified:   notes.txt 	modified:   qb(4).py

--- qb(4).py
+++ qb(4).py
@@ -12,4 +12,15 @@ def dir_list():
 dir_list()
 
 # copytree syntax & paramaters - shutil.copytree(src, dst, symlinks, ignore)
-shutil.copytree('/home/sherms/dir1', '/home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp), symlinks = False, ignore=shutil.ignore_patterns('/home/sherms/dir1/ignore1')) # it did not exclude the ignore1 folder
+print('\nCopying files...')
+# shutil.copytree('/home/sherms/dir1', '/home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp), symlinks = False, ignore=shutil.ignore_patterns('ignore1', 'src1')) # this works
+
+# folders = input('ignore1', 'src1') # folders to exclude - trying to work out a way to store folder names in a variable and use a single variable in ignore_patterns argument. This can let me print the folders that were excluded instead of manually typing them out.
+# I might not worry about this (abnove line) bc I need to finish this project. Been working on it for 6 months.
+
+exclude = shutil.ignore_patterns('ignore1', 'src1') # folders to exclude
+shutil.copytree('/home/sherms/dir1', '/home/sherms/{:%d%m%y-%H:%M:%S}'.format(stamp), ignore=exclude) # this works
+print('Finished copying files.')
+print("""Folders copied:
+         - src2
+         - t&cs.docx""")
